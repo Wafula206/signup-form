da1f5a50bc2c250d14e9d83118ee8fbf8bcf4cd2
Wafula206 <wafulabrian206@gmail.com>
2024-10-14 21:35:56 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>signup form</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+   <h1>WELCOME TO MY PAGE.</h1> 
+<div class="container">
+    <form action="">
+        <div>
+            <label for="Name">Name</label>
+            <input type="text" placeholder=" enter your name">
+            
+        </div>
+        <div>
+            <label for="Email">Email</label>
+            <input type="Email" placeholder=" enter your Email">
+            
+        </div>
+        <div class="genderContainer">
+            <label for="Gender">Gender</label>
+            <input class="Gender1" type="radio"name="Gende">Male
+            <input class="Gender1" type="radio"name="Gende">Female
+            <input class="Gender1" type="radio"name="Gende">Others
+        </div>
+        <div>
+            <label for="password">password</password>
+            <input type="text" placeholder=" enter your password">
+        </div>
+        <div>
+            <label for="Mobile">Mobile</label>
+            <input type="text" placeholder=" enter your Mobile">
+        </div>
+        <div class="btn">
+            <button type="submit">submit data</button>
+        </div>
+    </form>
+</div>
+<h2>designed by: brian wafula</h2>
+</body>
+</html>
